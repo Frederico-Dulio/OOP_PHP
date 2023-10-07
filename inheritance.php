7b6ca69378fc8e0fa29f97ce5159a4d5b929c9c5
Frederico Dulio <fredericodulio@gmail.com>
2023-10-08 00:39:08 +0100

    inheritance

---
+++ inheritance.php
@@ -0,0 +1,62 @@
+<?php
+
+class Person
+{
+    public $name;
+    public $age;
+    public $email;
+
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
+
+class Client extends Person
+{
+    public function buy()
+    {
+        //$this->age = "22-05-1999"; não faz sentido usar aqui
+
+        $this->age = new DateTime($this->age);
+        $to_day = new DateTime();
+        $your_age = $this->age->diff($to_day);
+        $age = $your_age->y;
+
+        return ($age >= 18) ? "{$this->name} você pode fazer compras:<br> Idade: $age <br> Email: {$this->email}" : "{$this->name} você é menor de idaide:<br> $age";
+    }
+}
+
+$cliente = new Client;
+$cliente->setName("Frederico Dulio");
+$cliente->setEmail("fredericodulio@gmail.com");
+$cliente->setAge("24-12-2000");
+
+echo $cliente->buy();
